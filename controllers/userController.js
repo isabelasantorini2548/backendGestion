dc0b9774ed7103e09dffe871ac6c6fcfa657aac7
isabelasantorini2548 <isabelasantorini2548@gmail.com>
2026-06-08 01:50:01 -0400

    fix the reloj

--- controllers/userController.js
+++ controllers/userController.js
@@ -514,6 +514,7 @@ const getComiteUser = asyncHandler(async (req, res) => {
       LEFT JOIN academico a ON u.idusuario = a.idusuario
       LEFT JOIN facultad f ON a.facultad_id = f.facultad_id
       WHERE u.role = 'academico' 
+        AND u.habilitado = true
       ORDER BY u.nombre, u.apellidopat
     `);
 
